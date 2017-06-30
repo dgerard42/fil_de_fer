e45a2d21c09a7737f7deef921aff79116d71df0d
Daniella Gerard <daniella4422@gmail.com>
2017-06-30 14:41:20 -0700

    starting norme

--- draw_line.c
+++ draw_line.c
@@ -12,38 +12,6 @@
 
 #include "fdf.h"
 
-/*
-void				color_setup(t_drw *drw, t_clr *clr)
-{
-	int local_clr_diff;
-
-	local_clr_diff = clr->start_color - clr->color_max;
-	if (abs(drw->rise) <= abs(drw->run))
-		clr->p_diff = (drw->x1 <= drw->x0) ? (drw->x0 - drw->x1) : (drw->x1 - drw->x0);
-	if (abs(drw->rise) > abs(drw->run))
-		clr->p_diff = (drw->y1 <= drw->y0) ? (drw->y0 - drw->y1) : (drw->y1 - drw->y0);
-	clr->c_bucket = 0;
-	clr->c_drop = (clr->p_diff != 0) ? (clr->color_diff / clr->p_diff) * 1000 : 1000;
-	clr->c_level = 1000;
-//	printf("after color setup z0=%d z1=%d\n", drw->z0, drw->z1);
-}
-
-
-int					color_inc(t_env *env, t_clr *clr, t_drw *drw)
-{
-	if (env->msize[2] > 0 && drw->z1 != drw->z0)
-	{
-		clr->c_bucket += clr->c_drop;
-		if (clr->c_bucket >= clr->c_level)
-		{
-			clr->start_color++;
-			clr->c_level += 1000;
-		}
-	}
-	return (clr->start_color);
-}
-*/
-
 void				draw_line(t_env *env, t_drw *drw)
 {
 	int		adjust;
@@ -56,7 +24,6 @@ void				draw_line(t_env *env, t_drw *drw)
 	drw->run = (drw->x1) - (drw->x0);
 	drop[0] = abs(drw->rise * 2);
 	drop[1] = abs(drw->run * 2);
-//	color_setup(drw, clr);
 	if (drw->run == 0)
 	{
 		if (drw->y1 < drw->y0)
--- draw_web.c
+++ draw_web.c
@@ -6,47 +6,23 @@
 /*   By: dgerard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 17:19:59 by dgerard           #+#    #+#             */
-/*   Updated: 2017/05/31 17:20:12 by dgerard          ###   ########.fr       */
+/*   Updated: 2017/06/30 12:55:37 by dgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/*
-void			color_calcs(t_env *env, t_drw *drw, t_clr *clr)
-{
-	clr->start_color = 0xFF0000;
-	if (env->msize[2] > 0)
-	{
-		float 	color_inc;
-		float	zdiff;
-
-		clr->end_color = 0x00FF00;
-		clr->color_diff = clr->start_color / clr->end_color;
-		// was originaly start color divided by end color. why did that work?
-		color_inc = clr->color_diff / (env->msize[2] * 1000 * (env->scale / 4));
-		zdiff = (drw->z0 > drw->z1) ? (drw->z0 - drw->z1) : (drw->z1 - drw->z0);
-		zdiff = (drw->z0 == drw->z1) ? 0 : zdiff;
-		//when these 2 lines are remved, the colors are far more brilliant
-		clr->color_max = zdiff * color_inc;
-	}
-}
-*/
-
 void			project(t_env *env, t_drw *drw)
 {
-	//printf("before color project calculations z0=%d z1=%d\n", drw->z0, drw->z1);
-	if (env->ps[0] < WIN_LEN && env->ps[1] < WIN_HI && env->ps[3] > 0 && env->ps[4] > 0)
+	if (env->ps[0] < WIN_LEN && env->ps[1] < WIN_HI && env->ps[3] > 0 &&
+			env->ps[4] > 0)
 	{
-//		color_calcs(env, drw, clr);
 		drw->x0 = env->ps[0];
 		drw->y0 = env->ps[1];
 		drw->x1 = env->ps[3];
 		drw->y1 = env->ps[4];
-		//printf("after color project caluclations z0=%d z1=%d\n", drw->z0, drw->z1);
 		draw_line(env, drw);
 	}
-//	printf("in drw-> x0=%d, y0=%d, x1=%d, y1=%d\n", drw->x0, drw->y0, drw->x1, drw->y1);
 }
 
 void			move(t_env *env)
@@ -60,14 +36,12 @@ void			move(t_env *env)
 		env->yoffset = 0;
 		env->zoffset = 0;
 	}
-//	printf("b4 offset ps 0->[%d] 1->[%d] 2->[%d] 3->[%d] 4->[%d] 5->[%d]\n", env->ps[0], env->ps[1], env->ps[2], env->ps[3], env->ps[4], env->ps[5]);
 	while (i < 6)
 	{
 		env->ps[i] = env->ps[i] + env->xoffset;
 		env->ps[i + 1] = env->ps[i + 1] + env->yoffset;
 		env->ps[i + 2] = env->ps[i + 2] + env->zoffset;
 		i = i + 3;
-//	printf("after offset ps 0->[%d] 1->[%d] 2->[%d] 3->[%d] 4->[%d] 5->[%d]\n", env->ps[0], env->ps[1], env->ps[2], env->ps[3], env->ps[4], env->ps[5]);
 	}
 }
 
@@ -82,7 +56,6 @@ void			rotate(t_env *env)
 		env->yrot = 0.2;
 		env->zrot = 0.0;
 	}
-	//	printf("xrot=%f, yrot=%f, zrot=%f", env->xrot, env->yrot, env->zrot);
 	while (i < 6)
 	{
 		if (env->xrot != 0)
@@ -100,7 +73,7 @@ void			rotate(t_env *env)
 			env->ps[i] = (env->ps[i] * cos(env->zrot)) + (env->ps[i + 1] * -sin(env->zrot));
 			env->ps[i + 1] = (env->ps[i] * sin(env->zrot)) + (env->ps[i + 1] * cos(env->zrot));
 		}
-		i = i + 3;
+		i += 3;
 	}
 }
 
@@ -116,22 +89,14 @@ void			scale(t_env *env, t_drw *drw)
 		env->winmax = (WIN_HI >= WIN_LEN) ? WIN_HI : WIN_LEN;
 		env->scale = env->winmax / (mapmax + 2);
 		env->zscale = ((env->msize[2] / mapmax) > 1) ? (env->msize[2] / mapmax) * 4 : 4;
-//		printf("msize[2] = %d, mapmax = %d\n", env->msize[2], env->mapmax);
-//		printf("winmax=%d, mapmax =%d", env->winmax, env->mapmax);
 	}
 	while (i < 6)
 	{
 		env->ps[i] = (env->ps[i] * env->scale) + env->scale;
 		env->ps[i + 1] = (env->ps[i + 1] * env->scale) + env->scale;
-//		printf("scale = %d, zscale = %d\n", env->scale, env->zscale);
 		env->ps[i + 2] = (env->ps[i + 2]) * (env->scale / env->zscale);
 		i = i + 3;
 	}
-//	printf("ps values ps[2]=%d, ps[5]=%d\n", env->ps[2], env->ps[5]);
-//	drw->z0 = (env->ps[2]) * 1000;
-//	drw->z1 = (env->ps[5]) * 1000;
-//	printf("immediately after filling in scale z0=%d z1=%d\n", drw->z0, drw->z1);
- //	printf("ps 0->[%d] 1->[%d] 2->[%d] 3->[%d] 4->[%d] 5->[%d]\n", env->ps[0], env->ps[1], env->ps[2], env->ps[3], env->ps[4], env->ps[5]);
 }
 
 void			draw_web(t_env *env)
@@ -140,7 +105,6 @@ void			draw_web(t_env *env)
 	int y;
 	int twice;
 	t_drw drw;
-//	t_clr clr;
 
 	twice = 0;
 	env->ps = ft_intarraynew(6);
@@ -152,9 +116,6 @@ void			draw_web(t_env *env)
 			x = 0;
 			while ((x + 1) < ((twice == 0) ? (env->msize[0]) : (env->msize[1])))
 			{
-	//			is_map(env->map);
-	//			printf("twice = %d\n", twice);
-	//			printf("x=%d, y=%d, z=%d\n", x, y, env->map[x][y]);
 				env->ps[0] = (twice == 0) ? x : y;
 				env->ps[1] = (twice == 0) ? y : x;
 				env->ps[2] = (twice == 0) ? env->map[y][x] : env->map[x][y];
@@ -162,11 +123,8 @@ void			draw_web(t_env *env)
 				env->ps[4] = (twice == 0) ? y : (x + 1);
 				env->ps[5] = (twice == 0) ? env->map[y][x + 1] : env->map[x + 1][y];
 				scale(env, &drw);
-	//			printf("before move z0=%d z1=%d\n", drw.z0, drw.z1);
 				move(env);
-	//			printf("before rotate z0=%d z1=%d\n", drw.z0, drw.z1);
 				rotate(env);
-	//			printf("before project z0=%d z1=%d\n", drw.z0, drw.z1);
 				project(env, &drw);
 				x++;
 			}
